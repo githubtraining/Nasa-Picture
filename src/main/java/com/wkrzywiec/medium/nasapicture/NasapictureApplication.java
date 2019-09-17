23e10e620d3943df3b90bbf11afc6fe72df4295f
John Marx <43185011+johndavidmarx@users.noreply.github.com>
2019-09-17 19:07:37 -0400

    Update NasapictureApplication.java

--- src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
+++ src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
@@ -2,34 +2,9 @@ package com.wkrzywiec.medium.nasapicture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @SpringBootApplication
-public class NasapictureApplication extends SpringBootServletInitializer {
- 
-    @Override
-    protected SpringApplicationBuilder configure(
-      SpringApplicationBuilder builder) {
-        return builder.sources(NasapictureApplication.class);
-    }
- 
-    public static void main(String[] args) {
-        SpringApplication sa = new SpringApplication(
-          NasapictureApplication.class);
-        sa.run(args);
-    }
- 
-    @RestController
-    public static class WarInitializerController {
- 
-        @GetMapping("/")
-        public String handler() {
-           // ...
-           return "Hello";
-        }
-    }
+public class NasapictureApplication {
+
+ 	public static void main(String[] args) { SpringApplication.run(NasapictureApplication.class, args); }
 }
