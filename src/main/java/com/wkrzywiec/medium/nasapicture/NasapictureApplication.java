a2cc471fa55ba037ca45b681c0b5734ac27ded95
John Marx <43185011+johndavidmarx@users.noreply.github.com>
2019-09-17 19:04:48 -0400

    Update NasapictureApplication.java

--- src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
+++ src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
@@ -29,6 +29,7 @@ public class NasapictureApplication extends SpringBootServletInitializer {
         @GetMapping("/")
         public String handler() {
            // ...
+           return "Hello";
         }
     }
 }
