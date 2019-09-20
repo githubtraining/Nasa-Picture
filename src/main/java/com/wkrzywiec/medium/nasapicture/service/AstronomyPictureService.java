8590ee0a240f5407ec7173be89cfb7bc954d47d6
John Marx <43185011+johndavidmarx@users.noreply.github.com>
2019-09-20 11:51:38 -0400

    Update AstronomyPictureService.java

--- src/main/java/com/wkrzywiec/medium/nasapicture/service/AstronomyPictureService.java
+++ src/main/java/com/wkrzywiec/medium/nasapicture/service/AstronomyPictureService.java
@@ -6,15 +6,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.date;
+
 @Service
 public class AstronomyPictureService {
 
     @Autowired
     private RestTemplate restTemplate;
+    
+    LocalDate startDate = LocalDate.of(2017, 1, 1); //start date
+    long start = startDate.toEpochDay();
 
+    LocalDate endDate = LocalDate.now(); //end date
+    long end = endDate.toEpochDay();
+
+    long randomEpochDay = ThreadLocalRandom.current().longs(start, end).findAny().getAsLong();
+
+    String pattern = "yyyy-MM-dd";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+    String date = simpleDateFormat.format(randomEpochDay);
+    
     public AstronomyPictureOfDay getTodayPicture() {
         return restTemplate.getForObject(
-                "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-07-12",
+                "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date="+date,
                 AstronomyPictureOfDay.class);
     }
 
